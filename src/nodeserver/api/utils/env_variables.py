8f454af37a00f45d63b494af747d29842b1e2fd0
yDewolf <andrersng@gmail.com>
2026-05-23 10:13:52 -0300

    Add a simple Project Root selector on server run

--- src/nodeserver/api/utils/env_variables.py
+++ src/nodeserver/api/utils/env_variables.py
@@ -1,9 +1,14 @@
 import os
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv, set_key
 
-from nodeserver.api.utils.file_utils import get_project_root
+from nodeserver.api.utils.file_utils import FileUtils, get_project_root
 
-load_dotenv()
+dotenv_path = find_dotenv(raise_error_if_not_found=True)
+load_dotenv(dotenv_path)
+project_root_path = os.getenv("PROJECT_ROOT_PATH")
+if not project_root_path:
+    FileUtils.select_project_root()
+    set_key(dotenv_path, "PROJECT_ROOT_PATH", str(FileUtils.project_root))
 
 secret_key = os.getenv("SECRET_KEY")
 SECRET_KEY = secret_key if secret_key else ""
--- src/nodeserver/api/utils/file_utils.py
+++ src/nodeserver/api/utils/file_utils.py
@@ -1,8 +1,40 @@
 from pathlib import Path
+import logging
+logger = logging.getLogger("root")
 
-def get_project_root() -> Path:
+def get_git_repos() -> list[Path]:
+    git_repos: list[Path] = []
     for parent in Path(__file__).resolve().parents:
         if (parent / '.git').exists():
-            return parent
+            git_repos.append(parent)
     
-    return Path(__file__).parent
+    return git_repos
+
+class FileUtils:
+    _project_root: Path = Path()
+
+    @property
+    def project_root(cls):
+        return cls._project_root
+
+    @project_root.setter
+    def project_root(cls, new_root: Path):
+        cls._project_root = new_root
+        logger.info(f"Set PROJECT ROOT to {cls._project_root}")
+    
+    @classmethod
+    def select_project_root(cls):
+        repos = get_git_repos()
+        for idx, repo_path in enumerate(repos):
+            print(f"[{idx}]-{repo_path}")
+        
+        idx = -1
+        while idx < 0 or idx > len(repos):
+            idx = int(input("Select which repository should be the project root: "))
+            if not idx < 0 and not idx > len(repos):
+                selected_path = repos[idx]
+        
+        cls.project_root = selected_path
+
+def get_project_root():
+    return FileUtils.project_root
